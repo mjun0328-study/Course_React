f3012fa1a6fc491bcca11967e95cfb2fe38aefc8
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-05-01 00:28:45 +0900

    Chapter 2 - 목차 구현하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <h1>Hello React!</h1>;
+  const index = ["HTML", "CSS", "JavaScript", "React"];
+  return (
+    <div>
+      <h1>Hello, WEB!</h1>
+      <ol>
+        {index.map((x, i) => {
+          return <li key={i}>{x}</li>;
+        })}
+      </ol>
+    </div>
+  );
 }
 
 export default App;

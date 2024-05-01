ef19745f3f5ee4eaef0c112d9724db6b0891025a
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-05-01 13:14:08 +0900

    Chapter 2 - 음미하기

--- src/App.js
+++ src/App.js
@@ -8,8 +8,18 @@ function App() {
           return <li key={i}>{x}</li>;
         })}
       </ol>
+      <Hello name="React" />
+      <Hello name="JavaScript" />
+      <Hello name="HTML" />
+      <Hello name="Node.js" />
+      <Hello name="WEB" />
+      <Hello name="HTTP" />
     </div>
   );
+}
+
+function Hello({ name }) {
+  return <p>Bye, {name}</p>;
 }
 
 export default App;

1cf3b618c61447720f7556ab79d7a9927146dc4a
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-05-10 14:32:16 +0900

    Chapter 5 - 나머지 코드 이주시키기

--- src/App.js
+++ src/App.js
@@ -21,6 +21,48 @@ function Nav({ index, setSelected, setMode }) {
   );
 }
 
+function View({ index, selected, setMode, setTopic }) {
+  return (
+    <>
+      <p>I love {index[selected]}</p>
+      <button
+        onClick={() => {
+          setMode("create");
+          setTopic("");
+        }}
+      >
+        Create
+      </button>
+    </>
+  );
+}
+
+function Create({ index, setIndex, setSelected, setMode, topic, setTopic }) {
+  return (
+    <>
+      <input
+        value={topic}
+        onChange={(e) => {
+          setTopic(e.target.value);
+        }}
+        placeholder="Enter new topic"
+      />
+      <button
+        onClick={() => {
+          setSelected(index.length);
+          setIndex((prev) => {
+            prev.push(topic);
+            return prev;
+          });
+          setMode("view");
+        }}
+      >
+        Submit
+      </button>
+    </>
+  );
+}
+
 function App() {
   const [index, setIndex] = useState(["HTML", "CSS", "JavaScript", "React"]);
   const [selected, setSelected] = useState(0);
@@ -32,40 +74,22 @@ function App() {
       <h1>Hello, WEB!</h1>
       <Nav index={index} setSelected={setSelected} setMode={setMode} />
       {mode === "view" && (
-        <>
-          <p>I love {index[selected]}</p>
-          <button
-            onClick={() => {
-              setMode("create");
-              setTopic("");
-            }}
-          >
-            Create
-          </button>
-        </>
+        <View
+          index={index}
+          selected={selected}
+          setMode={setMode}
+          setTopic={setTopic}
+        />
       )}
       {mode === "create" && (
-        <>
-          <input
-            value={topic}
-            onChange={(e) => {
-              setTopic(e.target.value);
-            }}
-            placeholder="Enter new topic"
-          />
-          <button
-            onClick={() => {
-              setSelected(index.length);
-              setIndex((prev) => {
-                prev.push(topic);
-                return prev;
-              });
-              setMode("view");
-            }}
-          >
-            Submit
-          </button>
-        </>
+        <Create
+          index={index}
+          setIndex={setIndex}
+          setSelected={setSelected}
+          setMode={setMode}
+          topic={topic}
+          setTopic={setTopic}
+        />
       )}
     </div>
   );
